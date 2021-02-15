2c95e97ff12a734b316fd50879beaa9cc748a266
dominicpoppe <d.poppe@infravider.com>
2021-02-15 13:00:20 +0100

    Added details to gameserver view. Improved the JS of it aswell.

--- assets/default/js/gameserver.js
+++ assets/default/js/gameserver.js
@@ -1,11 +1,23 @@
 if($("#panel").length > 0) {
     $(document).ready(function() {
         getLocations();
+        updatePriceTimer();
+        $("#location > option").each(function() {
+            if($(this).data("forge") != false && $(this).data("forge") != "") {
+                $('#location').val(this.value);
+                updatePrice();
+            }
+        });
     });
 
     const gameType = $("#panel").data("game-type");
     const selection = $("#panel").data("selection");
     const selectionType = $("#panel").data("selection-type");
+
+    let requiredCPU = 0;
+    let requiredRAM = 0;
+    let requiredDisc = 0;
+    let refreshTimer = 10;
 
     function updatePrice() {
         $(".priceLoader").css("display","block");
@@ -16,8 +28,33 @@ if($("#panel").length > 0) {
         }
         var result = callAPI("GET", "/ajax/gameserver/pricing?game=" + gameType + "&selection=" + selectionValue, { });
         $('#price').text(result.responseJSON.price.toFixed(2) + "€");
+        requiredCPU = result.responseJSON.requirements.cpu;
+        requiredRAM = result.responseJSON.requirements.ram;
+        requiredDisc = result.responseJSON.requirements.disc;
         getLocations();
         $(".priceLoader").css("display","none");
+        if($("#location").val() != "") {
+            updateHardware();
+        }
+        refreshTimer = 10;
+    }
+
+    function updatePriceTimer(){
+        if((refreshTimer - 1) <= 0) {
+            if(!$("#location").is(":focus")) {
+                if(!$("#selection").is(":focus")) {
+                    updatePrice();
+                    setTimeout(updatePriceTimer, 1000);
+                } else {
+                    refreshTimer = 10; setTimeout(updatePriceTimer, 1000);
+                }
+            } else {
+                refreshTimer = 10; setTimeout(updatePriceTimer, 1000);
+            }
+        } else {
+            refreshTimer -= 1;
+            setTimeout(updatePriceTimer, 1000);
+        }
     }
 
     $("#location").on('change', function() {
@@ -27,6 +64,18 @@ if($("#panel").length > 0) {
     $("#" + selection).on('change', function() {
         updatePrice();
     });
+
+    function updateHardware() {
+        if ($("#location").find(':selected').is('[data-forge]')) {
+            var forgeId = $("#location").find(':selected').data('forge');
+            var result = callAPI("GET", "/ajax/gameserver/getHardware?forgeId=" + forgeId, {});
+            $('#cpuDetails').html(requiredCPU + "x " + result.responseJSON.hardware.cpuSpeed + " GhZ (" + result.responseJSON.hardware.cpuModel + ")");
+            $('#ramDetails').html(requiredRAM + " MB " + result.responseJSON.hardware.ramType);
+            $('#discDetails').html((requiredDisc/1024) + " GB " + result.responseJSON.hardware.discType);
+            $('#networkDetails').html(result.responseJSON.hardware.network);
+            $('#ddosProtection').html(result.responseJSON.hardware.ddos);
+        }
+    }
 
     function getLocations() {
         if(selectionType === "dropdown") {
@@ -41,7 +90,9 @@ if($("#panel").length > 0) {
                 .filter(function (i, o) { return o.value === index; })
                 .length > 0;
             if(exists === false) {
-                $('#location').append('<option value="'+index+'">'+index+'</option>');
+                $('#location').append('<option data-forge="'+value+'" value="'+index+'">'+index+'</option>');
+            } else {
+                $('#location option[value="'+index+'"]').attr('data-forge', value);
             }
 
             if(value === false) {
@@ -51,10 +102,12 @@ if($("#panel").length > 0) {
             }
 
             //Check if button should be disabled or not depending on location
-            if($('#location option:selected').prop('disabled') == true){
-                $('#cartButton').attr('disabled', 'disabled');
-            } else {
+            if($('#location option:selected').prop('disabled') === false){
                 $('#cartButton').attr("disabled", false);
+            } else {
+                $('#cartButton').attr('disabled', 'disabled');
+                //Deselect the location
+                $("#location").val("");
             }
         });
     }
